ea0e9970a1976d847748fa64b54bf973d8cd8d0a
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2019-07-18 06:51:32 -0600

    Pokemon completed

--- server.js
+++ server.js
@@ -1,22 +1,52 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const app = express();
 
 const Pokemon = require('./models/pokemon');
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 // app.get('/pokemon', (req, res) => {
 // 	res.send(Pokemon);
 // });
 
+// Post
+app.post('/pokemon', (req, res) => {
+	Pokemon.push(req.body);
+	res.redirect('/pokemon')
+})
+
+// New
+app.get('/pokemon/new', (req, res) => {
+	res.render('new.ejs');
+});
+
+// Show
 app.get('/pokemon/:index', (req, res) => {
 	// console.log(req.params, "req params");
 	// console.log('/pokemon/:index');
 	res.render('show.ejs', {
-		pokemon: Pokemon[req.params.index]
+		pokemon: Pokemon[req.params.index],
+		index: req.params.index
 	})
 })
 
+// Edit
+app.get('/pokemon/:index/edit', (req, res) => {
+	res.render('edit.ejs', {
+		pokemon: Pokemon[req.params.index],
+		index: req.params.index
+	})
+});
+
+// Delete
+app.delete('/pokemon/:index', (req, res) => {
+	Pokemon.splice(req.params.index, 1);
+	res.redirect('/pokemon');
+})
+
+// Index
 app.get('/pokemon/', (req, res) => {
 	console.log(Pokemon)
 	res.render('index.ejs', {
@@ -24,6 +54,11 @@ app.get('/pokemon/', (req, res) => {
 	})
 })
 
+// Update
+app.put('/pokemon/:index', (req, res) => {
+	Pokemon[req.params.index] = req.body, 
+	res.redirect('/pokemon')
+})
 
 app.listen(3000, () =>{
 	console.log('my server is up and running');
